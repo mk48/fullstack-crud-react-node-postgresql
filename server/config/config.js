6cc8d18968086aedcb5d43133580cff59f3f4211
Kumaran <veerammalkumaran@gmail.com>
2019-03-27 21:46:22 +0530

    change pas

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: "postgres",
-        password: "Sony@2017",
+        password: "Sony",
         database: "store",
         host: "localhost",
         dialect: 'postgres',
